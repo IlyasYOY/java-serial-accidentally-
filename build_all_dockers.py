8ab45ababd2367b09e59d72d20757008aeda03a5
Ilia Ilinykh <ilyasyoy@gmail.com>
2022-06-04 11:56:33 +0300

    Some minor updates

--- build_all_dockers.py
+++ build_all_dockers.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
     print('🐳 Building Docker images')
     print("✋ Stopping all containers...")
     subprocess.run('docker stop $(docker ps -aq)', shell=True)
-
+    subprocess.run('docker rm -f $(docker ps -aq)', shell=True)
+    
     path = pathlib.Path('.')
 
     files = path.glob("Dockerfile.*")
@@ -20,7 +21,7 @@ if __name__ == '__main__':
         java_image_name = f"java-{ver}-test"
 
         print(f"🚮 Deleting image for {java_image_name} ")
-        subprocess.run(f'docker image rm {java_image_name}', shell=True)
+        subprocess.run(f'docker image rm -f {java_image_name}', shell=True)
 
         print(f"👷‍♂️ Building image for {java_image_name}")
         subprocess.run(f'docker build -f {file_name} . -t {java_image_name}', shell=True)
